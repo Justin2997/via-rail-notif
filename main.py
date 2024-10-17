891ae0875a087ba635aa1b10a8435ea798f8b72e
davpare <50345927+davpare@users.noreply.github.com>
2024-10-17 01:41:01 +0000

    Changement main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+  
+@app.get("/data/station")
+def fetch_data():
+    url = "https://tsimobile.viarail.ca/data/allData.json"
+    try:
+        response = requests.get(url, timeout=2.50)
+        response.raise_for_status()  # Raise an error for bad responses (4xx and 5xx)
+        data = response.json()  # Parse the JSON data
+        return data
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data: {e}")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
